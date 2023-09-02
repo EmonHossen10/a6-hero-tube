35307829b8abc1133f3ca6914998c68875f819d4
unknown <emonhossen1000@gmail.com>
2023-09-02 21:44:52 +0600

    data added

--- js/index.js
+++ js/index.js
@@ -19,43 +19,44 @@ const showCategory = (categories) => {
 };
 
 // handle load 4 data
-const handleLoadData =async (id) => {
-    const res=await fetch(`https://openapi.programming-hero.com/api/videos/category/${id}`);
-    const data=await res.json();
-    const mainData=data.data
-    console.log(mainData)
-  
-    
+const handleLoadData = async (id) => {
+  const res = await fetch(
+    `https://openapi.programming-hero.com/api/videos/category/${id}`
+  );
+  const data = await res.json();
+  const mainData = data.data;
+  console.log(mainData);
 
   const cardContainer = document.getElementById("card-container");
+  cardContainer.innerHTML="";
   // for each
   mainData.forEach((all) => {
+    console.log(all );
     const div = document.createElement("div");
     div.innerHTML = `
         <div class="card  bg-base-100 shadow-xl">
-        <figure><img src="https://shorturl.at/aCU25" alt="Shoes" /></figure>
+        <figure><img src="${all.thumbnail}" alt="Shoes" /></figure>
         <div class="card-body">
             <h2 class="card-title">
                 <figcaption class="flex items-center justify-center space-x-3">
-                    <img class="rounded-full w-9 h-9"
-                        src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/karen-nelson.png"
+                    <img   class="rounded-full w-9 h-9"
+                        src="${all.authors[0].profile_picture}"
                         alt="profile picture">
                     <div class="text-base space-y-0.5 font-bold dark:text-white text-left">
-                        <div>Building a Winning UX Strategy Using the Kano Model</div>
+                        <div>${all.title}</div>
                     </div>
                 </figcaption>
             </h2>
             <div class="flex justify-start ">
-                <p>Awlad Hossain</p>
-                <p class="mar">star</p>
+                <p><small> ${all.authors[0].profile_name} </small></p>
+                <p class="mar"> ${all.authors[0].verified ?  "Verified" :" not Verified"}  </p>
             </div>
-            <p>91k views</p>
+            <p>${all.others.views} views</p>
         </div>
     </div>
         `;
-        cardContainer.appendChild(div);
-    
-    
+    cardContainer.appendChild(div);
   });
 };
 loadData();
+handleLoadData(1000);
