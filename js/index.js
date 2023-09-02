30b44b476db20699c95a4afbb9bb89dfab0eb006
unknown <emonhossen1000@gmail.com>
2023-09-02 20:52:49 +0600

    dynamic data loadded in ui

---
+++ js/index.js
@@ -0,0 +1,61 @@
+const loadData = () => {
+  fetch("https://openapi.programming-hero.com/api/videos/categories")
+    .then((res) => res.json())
+    .then((data) => showCategory(data.data));
+};
+const showCategory = (categories) => {
+  console.log(categories);
+  const categoryContainer = document.getElementById("category-container");
+
+  categories.forEach((category) => {
+    // console.log(category.category_id);
+    const div = document.createElement("div");
+
+    div.innerHTML = `
+    <button onclick="handleLoadData('${category.category_id}')" class="btn btn-active">${category.category}</button>
+    `;
+    categoryContainer.appendChild(div);
+  });
+};
+
+// handle load 4 data
+const handleLoadData =async (id) => {
+    const res=await fetch(`https://openapi.programming-hero.com/api/videos/category/${id}`);
+    const data=await res.json();
+    const mainData=data.data
+    console.log(mainData)
+  
+    
+
+  const cardContainer = document.getElementById("card-container");
+  // for each
+  mainData.forEach((all) => {
+    const div = document.createElement("div");
+    div.innerHTML = `
+        <div class="card  bg-base-100 shadow-xl">
+        <figure><img src="https://shorturl.at/aCU25" alt="Shoes" /></figure>
+        <div class="card-body">
+            <h2 class="card-title">
+                <figcaption class="flex items-center justify-center space-x-3">
+                    <img class="rounded-full w-9 h-9"
+                        src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/karen-nelson.png"
+                        alt="profile picture">
+                    <div class="text-base space-y-0.5 font-bold dark:text-white text-left">
+                        <div>Building a Winning UX Strategy Using the Kano Model</div>
+                    </div>
+                </figcaption>
+            </h2>
+            <div class="flex justify-start ">
+                <p>Awlad Hossain</p>
+                <p class="mar">star</p>
+            </div>
+            <p>91k views</p>
+        </div>
+    </div>
+        `;
+        cardContainer.appendChild(div);
+    
+    
+  });
+};
+loadData();
